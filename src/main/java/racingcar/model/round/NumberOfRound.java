6db0ff838d999a29e627fe8179b6d3829c4c8ce6
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-07 15:50:41 +0900

    feature: Round 인터페이스, 이것의 구현체 클래스 NumberOfRound 구현 (#13)
    Round 인터페이스는 다음과 같은 시그니처의 메소드를 갖는다.
    hasNext 메소드는 플레이할 라운드가 남았는지 확인하는 방법이고
    next 메소드는 한 라운드 더 플레이할 때 한번 호출한다.
    ```java
    boolean hasNext();
    void next();
    ```
    
    NumberOfRound 는 시도할 횟수를 나타내는 일급 컬렉션 클래스이다.
    
    관련 일감
    [Round 인터페이스, 이것의 구현체 클래스 NumberOfRound 구현](https://github.com/OptimistLabyrinth/java-racingcar-precourse/issues/13)

---
+++ src/main/java/racingcar/model/round/NumberOfRound.java
@@ -0,0 +1,13 @@
+package racingcar.model.round;
+
+public class NumberOfRound implements Round {
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public void next() {
+
+    }
+}
