6535e4f38af8c5eb30120b6e25cc25320ea7576b
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-08 07:55:54 +0900

    feature: NumberOfRound 내부 로직 구현 (#54)
    아직 시도할 횟수가 남았으면 자동차 경주를 한 라운드 더 진행하도록 지시하고
    남은 시도 횟수가 0 이면 자동차 경주를 끝내야 한다고 지시한다.
    
    관련 일감
    [NumberOfRound 내부 로직 구현](https://github.com/OptimistLabyrinth/java-racingcar-precourse/issues/54)

--- src/main/java/racingcar/model/round/NumberOfRound.java
+++ src/main/java/racingcar/model/round/NumberOfRound.java
@@ -1,13 +1,19 @@
 package racingcar.model.round;
 
 public class NumberOfRound implements Round {
+    private int roundLeft;
+
+    public NumberOfRound(int roundLeft) {
+        this.roundLeft = roundLeft;
+    }
+
     @Override
     public boolean hasNext() {
-        return false;
+        return 0 < roundLeft;
     }
 
     @Override
     public void next() {
-
+        --roundLeft;
     }
 }
