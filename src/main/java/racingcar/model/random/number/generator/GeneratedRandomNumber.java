f94053c5eac74a931a809e117298a91c076c4abd
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-07 15:54:04 +0900

    feature: RandomNumberGenerator, GeneratedRandomNumber 클래스 구현 (#14)
    0 ~ 9 범위에서 무작위 숫자 하나를 고른다.
    이 무작위 숫자를 GeneratedRandomNumber 형태로 반환한다.
    
    관련 일감
    [RandomNumberGenerator, GeneratedRandomNumber 클래스 구현](https://github.com/OptimistLabyrinth/java-racingcar-precourse/issues/14)

---
+++ src/main/java/racingcar/model/random/number/generator/GeneratedRandomNumber.java
@@ -0,0 +1,9 @@
+package racingcar.model.random.number.generator;
+
+public class GeneratedRandomNumber {
+    private final int value;
+
+    public GeneratedRandomNumber(int value) {
+        this.value = value;
+    }
+}
