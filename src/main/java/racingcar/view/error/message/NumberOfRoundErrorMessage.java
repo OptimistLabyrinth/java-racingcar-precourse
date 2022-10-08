dd670418382ea3aa74564f49dfc4bcff3f18113b
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-08 14:49:32 +0900

    feature: NumberOfRoundErrorMessage 내부 로직 구현 (#71)
    사용자가 시도할 횟수 입력하는 과정에서 IllegalArgumentException 발생시키는 경우
    ```[ERROR]```로 시작하는 오류 메시지를 출력한다.
    
    관련 일감
    [NumberOfRoundErrorMessage 내부 로직 구현](https://github.com/OptimistLabyrinth/java-racingcar-precourse/issues/71)

--- src/main/java/racingcar/view/error/message/NumberOfRoundErrorMessage.java
+++ src/main/java/racingcar/view/error/message/NumberOfRoundErrorMessage.java
@@ -2,7 +2,7 @@ package racingcar.view.error.message;
 
 public class NumberOfRoundErrorMessage extends ErrorMessage {
     public NumberOfRoundErrorMessage(String value) {
-        super(value);
+        super("[ERROR] 시도할 횟수를 다시 입력해주세요.");
     }
 
     @Override
